2c599ceadefc7751ef83503ac80bb911b64262d5
SamanthaRmz <132226414+SamanthaRmz@users.noreply.github.com>
2024-03-10 20:47:45 -0600

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,28 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Usuario Administrativo
+        DB::table('users')->insert([
+            'name' => 'Admon',
+            'email' => 'admon@robotics.com',
+            'password' => Hash::make('Adm@2022'),
+            'role' => 'Administrativo',
+        ]);
+
+        // Usuario Docente
+        DB::table('users')->insert([
+            'name' => 'Tecmilenio',
+            'email' => 'tecmilenio@robotics.com',
+            'password' => Hash::make('Adm@2022'),
+            'role' => 'Docente',
+        ]);
+
+        // Usuario Estudiante
+        DB::table('users')->insert([
+            'name' => 'Estudiante',
+            'email' => 'estudiante@robotics.com',
+            'password' => Hash::make('Adm@2022'),
+            'role' => 'Estudiante',
+        ]);
     }
 }
